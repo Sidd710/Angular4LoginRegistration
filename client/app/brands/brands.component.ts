fb3822518119acc32108fa76252ca2359e31e302
Siddharth shah <sidd710@gmail.com>
2017-09-04 01:15:47 +0530

    css changes

--- client/app/brands/brands.component.ts
+++ client/app/brands/brands.component.ts
@@ -29,78 +29,33 @@ export class BrandsComponent {
       
 
 }
+// login() {
+//   this.fb.login()
+//     .then((res: LoginResponse) => {
+//       console.log('Logged in', res);
+//     })
+//     .catch(this.handleError);
+// }
 login() {
-  this.fb.login()
-    .then((res: LoginResponse) => {
-      console.log('Logged in', res);
-    })
-    .catch(this.handleError);
-}
+    
+        const loginOptions: LoginOptions = {
+          enable_profile_selector: true,
+          return_scopes: true,
+          scope: 'public_profile,user_friends,email,pages_show_list'
+        };
+    
+        this.fb.login(loginOptions)
+          .then((res: LoginResponse) => {
+            console.log('Logged in', res);
+          })
+          .catch(this.handleError);
+    
+      }
 twitterLogin()
 {
-  var TwitterStrategy = passporttwitter.Strategy;
-  passport.use(new TwitterStrategy({
-    
-            consumerKey: 'SvwQ4pp4jQUHMOKDctCBSI8iX',
-            consumerSecret: 'AExwLc5kssGaqmASrPyG0E0wmVS173wkHiyZAQkZ7EzTojmPrc',
-            callbackURL: 'http://127.0.0.1:8080/auth/twitter/callback'
-    
-        },
-            function (token, tokenSecret, profile, done) {
-    
-                // make the code asynchronous
-                // User.findOne won't fire until we have all our data back from Twitter
-                process.nextTick(function () {
-    
-                    userTwitterModel.findOne({ 'twitter.id': profile.id }, function (err, user) {
-    
-                        // if there is an error, stop everything and return that
-                        // ie an error connecting to the database
-                        if (err)
-                            return done(err);
-    
-                        // if the user is found then log them in
-                        if (user) {
-                            return done(null, user); // user found, return that user
-                        } else {
-                            // if there is no user, create them
-                            var newUser = new userTwitterModel();
-                            
-    
-                            // set all of the user data that we need
-                            newUser.twitter.id = profile.id;
-                            newUser.twitter.token = token;
-                            newUser.twitter.username = profile.username;
-                            newUser.twitter.displayName = profile.displayName;
-    
-                            // save our user into the database
-                            newUser.save(function (err) {
-                                if (err)
-                                    throw err;
-                                return done(null, newUser);
-                            });
-                        }
-                    });
-    
-                });
-    
-            }));
-  passporttwitter.get('/auth/twitter', passporttwitter.authenticate('twitter'));
+  console.log('twitterLogin');  
   
-    // handle the callback after twitter has authenticated the user
-    passporttwitter.get('/auth/twitter/callback',
-    passporttwitter.authenticate('twitter', {
-        successRedirect: '/profile',
-        failureRedirect: '/'
-      }));
-    // =====================================
-    // LOGOUT ==============================
-    // =====================================
-    passporttwitter.get('/logout', function (req, res) {
-      req.logout();
-      res.redirect('/');
-    });
-}
+};
 private handleError(error) {
   console.error('Error processing action', error);
 }
